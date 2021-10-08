416f32b1f2de4cda61247ea0275f440333cd3f77
Leo Lin <leolin@microsoft.com>
2021-10-07 23:56:42 -0700

    cooley tukey algorithm and test modification

--- src/fourier-transform.ts
+++ src/fourier-transform.ts
@@ -23,4 +23,45 @@ export default class FourierTransform {
 
     return frequencyPoints;
   };
+
+  cooleyTukeyAlgorithm = (sampledPoints: number[]): ComplexNumber[] => {
+    const N = sampledPoints.length;
+    if (N % 2 !== 0) {
+      throw Error("sampled points must be even for Cooley Tukey Algorithm");
+    } else if (N <= 2) {
+      return this.discreteFourierTransform(sampledPoints);
+    } else {
+      const sampledPointsEven = this.cooleyTukeyAlgorithm(
+        sampledPoints.filter((sampledPoint, index) => index % 2 === 0)
+      );
+
+      const sampledPointsOdd = this.cooleyTukeyAlgorithm(
+        sampledPoints.filter((sampledPoint, index) => index % 2 !== 0)
+      );
+
+      const factors = sampledPoints.map(
+        (point, k) =>
+          new ComplexNumber({
+            real: Math.cos((2 * Math.PI * k) / N),
+            imaginary: -1 * Math.sin((2 * Math.PI * k) / N),
+          })
+      );
+
+      const results = factors.map((factor, index) => {
+        if (index >= sampledPointsEven.length) {
+          // eslint-disable-next-line no-param-reassign
+          index -= sampledPointsEven.length;
+        }
+        return sampledPointsEven[index]
+          .add(factor)
+          .multiply(sampledPointsOdd[index]);
+      });
+
+      return results;
+    }
+  };
+
+  private primeFactorAlgorithm = (
+    sampledPoints: number[]
+  ): ComplexNumber[] => [];
 }
--- tests/fourier-transform.test.ts
+++ tests/fourier-transform.test.ts
@@ -1,19 +1,16 @@
 import ComplexNumber from "../src/complex-number";
 import FourierTransform from "../src/fourier-transform";
 
-describe("Should be able to transform sampled amplitude points", () => {
-  let fourierTransform: FourierTransform;
-  beforeEach(() => {
-    fourierTransform = new FourierTransform();
-  });
-  test("Should be able to transform 0 sampled amplitude points", () => {
+const commonFourierTransformTests = (
+  implementationName: string,
+  implementation: any
+) => {
+  test(`Should be able to transform 0 sampled amplitude points using ${implementationName}`, () => {
     const sampledPoints: number[] = [];
-    expect(
-      fourierTransform.discreteFourierTransform(sampledPoints)
-    ).toStrictEqual([]);
+    expect(implementation(sampledPoints)).toStrictEqual([]);
   });
 
-  test("Should be able to sample many amplitude points, simple example", () => {
+  test(`Should be able to sample many amplitude points using ${implementationName}, simple example`, () => {
     const sampledPoints: number[] = [0, 0.707, 1, 0.707, 0, -0.707, -1, -0.707];
     const expectedResults: ComplexNumber[] = [
       new ComplexNumber({ real: 0, imaginary: 0 }),
@@ -26,8 +23,7 @@ describe("Should be able to transform sampled amplitude points", () => {
       new ComplexNumber({ real: 0, imaginary: 4 }),
     ];
 
-    const actualResults =
-      fourierTransform.discreteFourierTransform(sampledPoints);
+    const actualResults: ComplexNumber[] = implementation(sampledPoints);
 
     actualResults.forEach((actualResult, index) => {
       expect(actualResult.real).toBeCloseTo(expectedResults[index].real);
@@ -37,7 +33,7 @@ describe("Should be able to transform sampled amplitude points", () => {
     });
   });
 
-  test("Should be able to sample many amplitude points, complex eample", () => {
+  test(`Should be able to sample many amplitude points using ${implementationName}, complex eample`, () => {
     const sampledPoints: number[] = [
       0.2165, 0.8321, 0.7835, 0.5821, 0.2165, -0.5821, -1.2165, -0.8321,
     ];
@@ -52,8 +48,7 @@ describe("Should be able to transform sampled amplitude points", () => {
       new ComplexNumber({ real: 0, imaginary: 4 }),
     ];
 
-    const actualResults =
-      fourierTransform.discreteFourierTransform(sampledPoints);
+    const actualResults: ComplexNumber[] = implementation(sampledPoints);
 
     actualResults.forEach((actualResult, index) => {
       expect(actualResult.real).toBeCloseTo(expectedResults[index].real);
@@ -62,4 +57,20 @@ describe("Should be able to transform sampled amplitude points", () => {
       );
     });
   });
+};
+
+describe("Should be able to transform sampled amplitude points using Discrete Fourier Transform", () => {
+  const fourierTransform = new FourierTransform();
+  commonFourierTransformTests(
+    "Discrete Fourier Transform",
+    fourierTransform.discreteFourierTransform
+  );
+});
+
+describe("Should be able to transform sampled amplitude points using Cooley-Tukey Algorithm", () => {
+  const fourierTransform = new FourierTransform();
+  commonFourierTransformTests(
+    "Cooley-Tukey Algorithm",
+    fourierTransform.cooleyTukeyAlgorithm
+  );
 });
